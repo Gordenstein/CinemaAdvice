fad9484caddfab65c1d3685d5a21081806d5ebc2
Eugene <gordeev-96@yandex.ru>
2018-04-11 19:28:16 +0300

    Initial Commit

---
+++ CinemaAdvice/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CinemaAdvice
+//
+//  Created by Hero on 11.04.2018.
+//  Copyright © 2018 Eugene Gordeev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
