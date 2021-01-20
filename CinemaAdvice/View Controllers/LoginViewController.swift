91ba74b3650b90824cd01835d804df0bfef5e048
Eugene Gordenstein <e.gordenstein@gmail.com>
2021-01-20 12:17:32 +0200

    Pods update and LoginViewController refactoring

--- CinemaAdvice/View Controllers/LoginViewController.swift
+++ CinemaAdvice/View Controllers/LoginViewController.swift
@@ -11,9 +11,8 @@ import Firebase
 
 class LoginViewController: UIViewController {
   
-  // MARK: Constants
   let loginToList = "LoginToList"
-  var fitstTime = true
+  var checkEmailVerification = false
   
   // MARK: Outlets
   @IBOutlet weak var textFieldLoginEmail: UITextField!
@@ -21,21 +20,15 @@ class LoginViewController: UIViewController {
   @IBOutlet weak var loginButton: UIButton!
   @IBOutlet weak var signUpButton: UIButton!
   
-  
   override func viewDidLoad() {
     super.viewDidLoad()
     signUpButton.layer.borderWidth = 1
-    if #available(iOS 13.0, *) {
-        signUpButton.layer.borderColor = UIColor.label.cgColor
-    } else {
-        // Fallback on earlier versions
-    }
+    signUpButton.layer.borderColor = UIColor.label.cgColor
     self.textFieldLoginEmail.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor : UIColor(named:"palceholder_color") ?? .yellow])
     self.textFieldLoginPassword.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor(named:"palceholder_color") ?? .yellow])
     
     Auth.auth().addStateDidChangeListener() { auth, user in
       if user != nil {
-        
         self.performSegue(withIdentifier: self.loginToList, sender: nil)
         self.textFieldLoginEmail.text = nil
         self.textFieldLoginPassword.text = nil
@@ -48,26 +41,15 @@ class LoginViewController: UIViewController {
   }
   
   @IBAction func loginDidTouch(_ sender: Any) {
-    fitstTime = false
-    guard
-      let email = textFieldLoginEmail.text,
-      let password = textFieldLoginPassword.text,
-      email.count > 0,
-      password.count > 0
-      else {
-        return
+    guard let email = textFieldLoginEmail.text,
+          let password = textFieldLoginPassword.text,
+          email.count > 0,
+          password.count > 0 else {
+      return
     }
-    Auth.auth().signIn(withEmail: email, password: password) { user, error in
-      if let error = error, user == nil {
-        let alert = UIAlertController(title: NSLocalizedString("Authorisation Error", comment: "Localized kind: Ошибка авторизации"),
-                                      message: error.localizedDescription,
-                                      preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Localized kind: ОК"), style: .default))
-        self.present(alert, animated: true, completion: nil)
-      }
-      if user != nil {
-        if (user?.isEmailVerified ?? false) || true {
+    Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+      if let authResult = authResult {
+        if !self.checkEmailVerification || authResult.user.isEmailVerified {
           self.performSegue(withIdentifier: self.loginToList, sender: nil)
           self.textFieldLoginEmail.text = nil
           self.textFieldLoginPassword.text = nil
@@ -75,14 +57,21 @@ class LoginViewController: UIViewController {
           let alert = UIAlertController(title: NSLocalizedString("Login is not available", comment: "Localized kind: На данный момент вход невозможен"),
                                         message: NSLocalizedString("A verification link has been send to your email account. Please click on the link that has been sent to your email.", comment: "Localized kind: Ваш email был зарегистрирован, но для входа необходимо его подтвердить. Пожалуйста, перейдите по ссылке в письме."),
                                         preferredStyle: .alert)
-          
           alert.addAction(UIAlertAction(title: NSLocalizedString("Got it!", comment: "Localized kind: Хорошо!"), style: .default))
           self.present(alert, animated: true, completion: nil)
-          
         }
+      } else {
+        var errorMessage = "Please try again later"
+        if let error = error {
+          errorMessage = error.localizedDescription
+        }
+        let alert = UIAlertController(title: NSLocalizedString("Authorisation Error", comment: "Localized kind: Ошибка авторизации"),
+                                      message: errorMessage,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Localized kind: ОК"), style: .default))
+        self.present(alert, animated: true, completion: nil)
       }
     }
-    
   }
   
   @IBAction func signUpDidTouch(_ sender: Any) {
@@ -92,26 +81,30 @@ class LoginViewController: UIViewController {
     
     let saveAction = UIAlertAction(title: NSLocalizedString("Save", comment: "Localized kind: Сохранить"),
                                    style: .default) { action in
-                                    let emailField = alert.textFields![0]
-                                    let passwordField = alert.textFields![1]
-                                    self.fitstTime = false
-                                    Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!, completion: { (user, error) in
-                                      if error != nil {
-                                        if let errorCode = AuthErrorCode(rawValue: error!._code) {
-                                          switch errorCode {
-                                          case .weakPassword:
-                                            print("Please provide a strong password")
-                                          default:
-                                            print("There is an error")
-                                          }
-                                        }
-                                      }
-                                      if user != nil {
-                                        user?.sendEmailVerification(completion: { (error) in
-                                          print(error?.localizedDescription ?? "No verification")
-                                        })
-                                      }
-                                    })
+      guard let textFields = alert.textFields,
+            textFields.count > 1,
+            let email = textFields[0].text,
+            let password = textFields[1].text else {
+        // Error
+        return
+      }
+      Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+        if let error = error, let errorCode = AuthErrorCode(rawValue: error._code) {
+          switch errorCode {
+          case .weakPassword:
+            print("User set weak password")
+          default:
+            print("Sing Up error: \(errorCode)")
+          }
+        }
+        if let user = authResult?.user {
+          user.sendEmailVerification { (error) in
+            print(error?.localizedDescription ?? "No verification")
+          }
+        } else {
+          // Error
+        }
+      }
     }
     let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: "Localized kind: Отмена"),style: .default)
     alert.addTextField { textEmail in
